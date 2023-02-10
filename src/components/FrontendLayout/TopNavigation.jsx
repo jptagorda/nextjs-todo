aa7893523132a8ff2589255e09ed522a31dda1b5
jxclsv <justinxclusive@gmail.com>
2023-02-10 12:24:45 +0800

    updated top nav

--- src/components/FrontendLayout/TopNavigation.jsx
+++ src/components/FrontendLayout/TopNavigation.jsx
@@ -39,7 +39,7 @@ export default function TopNavigation() {
                         ))}
                     </Popover.Group>
                     <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-                        {/* <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+                        <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
                             Sign in
                         </a>
                         <a
@@ -47,7 +47,7 @@ export default function TopNavigation() {
                             className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 text-base font-medium text-white shadow-sm hover:from-purple-700 hover:to-indigo-700"
                         >
                             Sign up
-                        </a> */}
+                        </a>
                     </div>
                 </div>
 
