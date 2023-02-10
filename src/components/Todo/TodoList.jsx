5d35634747795ef41e2a3de964ff8d6155344a8b
jxclsv <justinxclusive@gmail.com>
2023-02-10 10:24:02 +0800

    prettier

--- src/components/Todo/TodoList.jsx
+++ src/components/Todo/TodoList.jsx
@@ -1,8 +1,6 @@
-import { useState } from 'react';
 import TodoItem from './TodoItem';
 
 export default function TodoList({ todos, setTodos }) {
-
     return (
         <ul role="list" className="divide-y divide-gray-200">
             {todos.map(todo => (
