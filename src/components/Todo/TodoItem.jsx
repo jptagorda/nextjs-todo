8f8399e3b2ef00a83cd507be63cfba47b8aaac08
jxclsv <justinxclusive@gmail.com>
2023-02-10 11:12:20 +0800

    merged todo edit input component to todo item

--- src/components/Todo/TodoItem.jsx
+++ src/components/Todo/TodoItem.jsx
@@ -1,26 +1,62 @@
-import { PencilSquareIcon, XCircleIcon } from '@heroicons/react/24/outline';
+import { CheckCircleIcon, PencilSquareIcon, XCircleIcon } from '@heroicons/react/24/outline';
 import { useState } from 'react';
-import TodoEditInput from './TodoEditInput';
+import TextInput from '../Form/TextInput';
 
 export default function TodoItem({ todo, setTodos }) {
     const [editing, setEditing] = useState(false);
+    const [newTodo, setNewTodo] = useState(todo.value);
+
+    const updateTodo = event => {
+        event.preventDefault();
+
+        setTodos(prevTodos => {
+            const todoIndex = prevTodos.findIndex(prev => prev.id === todo.id);
+            prevTodos[todoIndex].value = newTodo;
+            return prevTodos;
+        });
+
+        setEditing(false);
+    };
 
     return (
         <li key={todo.id} className="flex items-center justify-between py-6">
             {editing ? (
                 <>
-                    <TodoEditInput todo={todo} setTodos={setTodos} setEditingTodo={setEditing} />
-                    <button
-                        type="button"
-                        onClick={() => setEditing(false)}
-                        className="ml-24 inline-flex items-center rounded-full border border-transparent bg-indigo-600 p-2 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                    >
-                        <XCircleIcon className="h-5 w-5" />
-                    </button>
+                    <form onSubmit={updateTodo} className="w-full">
+                        <TextInput
+                            id="update-todo"
+                            name="update-todo"
+                            value={newTodo}
+                            type="update-todo"
+                            className="w-full !border-indigo-500 !py-1.5"
+                            autoComplete="update-todo"
+                            handleChange={e => setNewTodo(e.target.value)}
+                            required
+                            isFocused
+                            placeholder="+ Add task and Press Enter to save."
+                        />
+                    </form>
+
+                    <div className="ml-24 flex items-center space-x-3">
+                        <button
+                            type="button"
+                            onClick={updateTodo}
+                            className="inline-flex items-center rounded-full border border-transparent bg-green-600 p-2 text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+                        >
+                            <CheckCircleIcon className="h-5 w-5" />
+                        </button>
+                        <button
+                            type="button"
+                            onClick={() => setEditing(false)}
+                            className="inline-flex items-center rounded-full border border-transparent bg-indigo-600 p-2 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                        >
+                            <XCircleIcon className="h-5 w-5" />
+                        </button>
+                    </div>
                 </>
             ) : (
                 <>
-                    <p className="ml-3 text-sm font-medium text-gray-900">{todo.value}</p>
+                    <p className="ml-3 text-lg font-medium text-gray-900">{todo.value}</p>
 
                     <div className="space-x-3">
                         <button
